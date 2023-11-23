ea1bc8afe72bc58dfb4aa039fb198cc3693a21c1
boysimon10 <simondiouf2710@gmail.com>
2023-11-23 13:19:01 +0000

    bootstrap-react config

--- src/App.js
+++ src/App.js
@@ -1,53 +1,80 @@
 import React from 'react';
-import { Navbar, Container, Nav, Card, Row, Col } from 'react-bootstrap';
+import { Navbar, Container, Nav, Card, Row, Col, Form, NavDropdown, Button } from 'react-bootstrap';
 
 function App() {
   return (
     <>
       <div className="App">
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 
         <Container className="mt-4">
-          <h1>React Bootstrap Cards Example</h1>
           <Row>
             <Col>
-              <Card style={{ width: '18rem' }}>
+              <Card style={{ width: '18rem' }} className="mb-3">
                 <Card.Img variant="top" src="https://via.placeholder.com/150" />
                 <Card.Body>
                   <Card.Title>Card 1</Card.Title>
                   <Card.Text>
-                    This is a sample card with some text content. You can add more details here.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac arcu vitae velit lacinia finibus. 
                   </Card.Text>
                 </Card.Body>
               </Card>
             </Col>
             <Col>
-              <Card style={{ width: '18rem' }}>
+              <Card style={{ width: '18rem' }} className="mb-3">
                 <Card.Img variant="top" src="https://via.placeholder.com/150" />
                 <Card.Body>
                   <Card.Title>Card 2</Card.Title>
                   <Card.Text>
-                    Another sample card with some text content. You can customize this card as needed.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac arcu vitae velit lacinia finibus. 
                   </Card.Text>
                 </Card.Body>
               </Card>
             </Col>
             <Col>
-              <Card style={{ width: '18rem' }}>
+              <Card style={{ width: '18rem' }} className="mb-3">
                 <Card.Img variant="top" src="https://via.placeholder.com/150" />
                 <Card.Body>
                   <Card.Title>Card 3</Card.Title>
                   <Card.Text>
-                    Yet another sample card with some text content. Feel free to modify it.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac arcu vitae velit lacinia finibus. 
                   </Card.Text>
                 </Card.Body>
               </Card>
