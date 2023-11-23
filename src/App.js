b72d3cd05298e82b3cc3e6a452d3e6ff97e919eb
boysimon10 <simondiouf2710@gmail.com>
2023-11-23 00:50:14 +0000

    bootstrap npm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Navbar, Container, Nav, Card, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        <Container className="mt-4">
+          <h1>React Bootstrap Cards Example</h1>
+          <Row>
+            <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                <Card.Body>
+                  <Card.Title>Card 1</Card.Title>
+                  <Card.Text>
+                    This is a sample card with some text content. You can add more details here.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                <Card.Body>
+                  <Card.Title>Card 2</Card.Title>
+                  <Card.Text>
+                    Another sample card with some text content. You can customize this card as needed.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                <Card.Body>
+                  <Card.Title>Card 3</Card.Title>
+                  <Card.Text>
+                    Yet another sample card with some text content. Feel free to modify it.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
   );
 }
 
